f5690ca108afe62e2bc8153ab6d91d290ce35261
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-09-26 17:04:01 +0300

    feat(): added traceProvider

--- Observability.ConsoleApp/Program.cs
+++ Observability.ConsoleApp/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using Observability.ConsoleApp;
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 Console.WriteLine("Hello, World!");
+
+var traceProvider = Sdk.CreateMeterProviderBuilder()
+    .ConfigureResource(configure =>
+    {
+        configure.AddService(OpenTelemetryConstant.ServiceName, OpenTelemetryConstant.ServiceVersion)
+        .AddAttributes(new List<KeyValuePair<string, object>>()
+        {
+            new KeyValuePair<string ,object>("host.machineName",
+            Environment.MachineName),
+            new KeyValuePair<string, object>("host.environment",
+            "dev")
+        });
+    }).Build();
+
+
+
+
