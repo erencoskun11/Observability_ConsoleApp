3187bea1bbcd01c3dbd4cda7b064df0c56d384ea
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-09-26 19:01:03 +0300

    feat(ServiceOne):

---
+++ Observability.ConsoleApp/ServiceOne.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observability.ConsoleApp
+{
+    internal class ServiceOne
+    {
+        static HttpClient httpClient = new HttpClient();
+        internal async Task<int> MakeRequestToGoogle()
+        {
+            using var activity = ActivitySourceProvider.Source.StartActivity();
+
+            var result = await httpClient.GetAsync("http://www.google.com");
+            
+            var responseContent = await result.Content.ReadAsStringAsync();
+
+            return responseContent.Length;
+
+        }
+
+
+    }
+}
